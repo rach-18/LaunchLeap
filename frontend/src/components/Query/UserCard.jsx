4ebfef81f7d45d0e21346ad80d26d476bd8126b0
rach-18 <rachitapradhan448@gmail.com>
2024-11-24 01:42:52 +0530

    Made the Response page responsive

--- frontend/src/components/Query/UserCard.jsx
+++ frontend/src/components/Query/UserCard.jsx
@@ -1,14 +1,8 @@
 function UserCard({ field, value, type }) {
     return (
-        <div className={`p-8 border-2 border-gray-200 rounded-lg shadow-lg bg-white w-full ${type === 'user' ? 'mt-2' : 'mt-8'}`}>
-            <div className="">
-                <p className='text-lg font-bold mb-2'>
-                    {field}
-                </p>
-                <p className='text-gray-500'>
-                    {value}
-                </p>
-            </div>
+        <div className="p-4 sm:p-6 border-2 border-gray-200 rounded-lg shadow-lg bg-white">
+            <p className='font-bold mb-2 text-sm sm:text-base'>{field}</p>
+            <p className='text-gray-600 text-sm sm:text-base whitespace-pre-wrap'>{value}</p>
         </div>
     )
 }
